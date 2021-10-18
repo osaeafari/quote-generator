03ab8e2fc5db74846f2aff5350e8d4b7f5546a86
Kwame Osae Afari <kayfari20@gamil.com>
2021-10-18 12:14:47 +0000

    added fetch quote API to script file

---
+++ script.js
@@ -0,0 +1,23 @@
+//let apiQuotes = [];
+
+// Show new  Quote
+function newQuote() {
+    // Pick a random quote from apiQuotes array
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    console.log(quote);
+}
+
+// Get Quotes From API
+// async function getQuotes() {
+//     const apiUrl = 'https://type.fit/api/quotes';
+//     try {
+//         const response =  await fetch(apiUrl);
+//         apiQuotes = await response.json();
+//         newQuote();
+//     } catch (error) {
+//         //Catch Error
+//     }
+// }
+// // ON Load
+// getQuotes();
+newQuote();
