2d15d71f95d8621097bd2e78dc03a7e0725d5897
Kwame Osae Afari <kayfari20@gamil.com>
2021-10-18 12:34:07 +0000

    added functionalities to buttons, incoperated tweets and new Quote fetch from quote API

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
-//let apiQuotes = [];
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+
+let apiQuotes = [];
+
 
 // Show new  Quote
 function newQuote() {
     // Pick a random quote from apiQuotes array
-    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    console.log(quote);
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // Check is author field is blank and replace it with unknown
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    //Check Quote length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
-// async function getQuotes() {
-//     const apiUrl = 'https://type.fit/api/quotes';
-//     try {
-//         const response =  await fetch(apiUrl);
-//         apiQuotes = await response.json();
-//         newQuote();
-//     } catch (error) {
-//         //Catch Error
-//     }
-// }
-// // ON Load
-// getQuotes();
-newQuote();
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response =  await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        //Catch Error
+    }
+}
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// ON Load
+getQuotes();
